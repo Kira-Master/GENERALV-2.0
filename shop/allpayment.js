b7c7e0ffcf8d0075107217791c874da58b42942d
Kira-Master <fauziponcol21@gmail.com>
2021-10-27 19:31:17 +0700

    GENERAL V2.0

---
+++ shop/allpayment.js
@@ -0,0 +1,25 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY KIRA MASTER
+	
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0823 2215 3431
+		• FAUZI
+2. OVO
+	ㅁ 0823 2215 3432
+		• FAUZI
+3. DANA
+	ㅁ 0823 2215 3431
+		• R***I
+
+*さ PULSA*
+
+1. TELKOMSEL
+	ㅁ 0823 2215 3431
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
